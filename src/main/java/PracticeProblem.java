fe0b4cc52b8b6e2ade6b83d2cda6f9be690ec600
michael-chen-ics4u1-01-25 <michael.chen29@student.tdsb.on.ca>
2025-09-19 13:07:51 -0400

    completed set

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,5 +4,35 @@ public class PracticeProblem {
 
 	}
 
+	public static int[] createIntArray() {
+		int[] arr = {1, 2, 3, 4, 5};
+		return arr;
+	}
 
+	public static String[] createArray(String a, String b, String c, String d) {
+		String[] arr = {a, b, c, d};
+		return arr;
+	}
+
+	public static int findValue(int a, int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == a) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public static int findThirdValue(String a, String[] arr) {
+		int count = 0;
+		for(int i = 0; i < arr.length; i++) {
+			if (arr[i] == a) {
+				count += 1;
+			}
+			if (count == 3) {
+				return i;
+			}
+		}
+		return -1;
+	}
 }
